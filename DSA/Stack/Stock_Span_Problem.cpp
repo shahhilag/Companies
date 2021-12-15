6e7cad404bfec0e9af86b3b44cf1e2ff0adff28d
shahhilag <shahhilag16@gmail.com>
2021-12-15 14:54:03 +0530

    Stock Span updated

--- DSA/Stack/Stock_Span_Problem.cpp
+++ DSA/Stack/Stock_Span_Problem.cpp
@@ -7,47 +7,34 @@ void dfile()
      cin.tie(NULL);
 } 
 
-// vector<int> stockSpanner(vector<int> &stocks)
-// {
-//     int n=stocks.size();
-//     int counter=0;
-//     vector<int> vcounter;
-//     stack<int> s;
-//     for(int i=0;i<n;i++)
-//     {
-//         s.push(stocks[i]);
-//         for(int j=0;j<=i;j++)
-//         {
-//             if(stocks[i]>stocks[j])
-//             {
-//                 s.push(stocks[j]);
-//             }
-//         }
-//         while(!s.empty())
-//         {
-//             counter++;
-//             s.pop();
-//         }
-//         vcounter.push_back(counter);
-//         counter=0;
-//     }    
-//     return vcounter;
-// }
-vector<int> stockSpanner(vector<int> &stocks){
-	stack <int> s;
-	int n = stocks.size();
-	s.push(0);
-	vector<int> arr(n, 1);
-	for (int i = 1; i < n; i++) {
-		while (!s.empty() and stocks[s.top()] <= stocks[i]) {
-			s.pop();
-		}
-		if (!s.empty()) {
-			arr[i] = i - s.top();
-		} else arr[i] = i + 1;
-		s.push(i);
-	}
-	return arr;
+vector<int> stockSpanner(vector<int> &stocks)
+{
+    int n=stocks.size();
+    int counter=0;
+    vector<int> vcounter;
+    stack<int> s;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i;j>=0;j--)
+        {
+            if(stocks[i]>=stocks[j])
+            {
+                s.push(stocks[j]);
+            }
+            else if(stocks[i]<stocks[j])
+            {
+                break;
+            }
+        }
+        while(!s.empty())
+        {
+            counter++;
+            s.pop();
+        }
+        vcounter.push_back(counter);
+        counter=0;
+    }    
+    return vcounter;
 }
 
 int main()
